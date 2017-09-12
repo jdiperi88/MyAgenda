adb7e63d0931891ebe1312afb34dabe70584d91e
Joseph DiPeri <jdiperi88@gmail.com>
2017-09-12 11:10:04 -0400

    switched to a postgres database

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,16 @@
 
 ActiveRecord::Schema.define(version: 20170908035136) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.string "task"
     t.string "duration"
     t.string "quantity"
     t.string "notes"
     t.string "completed"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
@@ -33,4 +36,5 @@ ActiveRecord::Schema.define(version: 20170908035136) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tasks", "users"
 end
